8f4d033875cdd2b5949361f0fe5cb76306e64d4a
Luis Marcano <luissilvam537@gmail.com>
2026-05-09 00:08:45 -0300

    Separate contact entity mapping configuration

--- src/MedgrupoChallenge/Infrastructure/Data/AppDbContext.cs
+++ src/MedgrupoChallenge/Infrastructure/Data/AppDbContext.cs
@@ -1,49 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MedgrupoChallenge.Domain.Entities;
+﻿using MedgrupoChallenge.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace MedgrupoChallenge.Infrastructure.Data;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) 
         : base(options)
     {
     }
-    
+
     public DbSet<Contact> Contacts => Set<Contact>();
-    
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
         base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Contact>(entity =>
-        {
-            entity.ToTable("Contacts");
-
-            entity.HasKey(contact => contact.Id);
-
-            entity.Property(contact => contact.Name)
-                .IsRequired()
-                .HasMaxLength(150);
-
-            entity.Property(contact => contact.BirthDate)
-                .IsRequired()
-                .HasColumnType("date");
-
-            entity.Property(contact => contact.Gender)
-                .IsRequired()
-                .HasConversion<int>();
-
-            entity.Property(contact => contact.IsActive)
-                .IsRequired();
-
-            entity.Property(contact => contact.CreatedAt)
-                .IsRequired();
-
-            entity.Property(contact => contact.UpdatedAt)
-                .IsRequired(false);
-
-            entity.Ignore(contact => contact.Age);
-        });
     }
 }
